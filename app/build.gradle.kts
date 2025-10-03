bbe7f8ecbdd93c1acdeaeb143a6c640731283eba
Bartek <bartlomiej00wilk@gmail.com>
2025-10-03 13:09:38 +0200

    room + dao added, changed User class and added database impelemntation for login/register

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,16 +2,17 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27"
 }
 
 android {
     namespace = "wolf.north.sitzer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "wolf.north.sitzer"
-        minSdk = 24
         targetSdk = 34
+        minSdk = 21
         versionCode = 1
         versionName = "1.0"
 
@@ -56,7 +57,7 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.3.0")
     implementation("androidx.compose.material3:material3-window-size-class:1.3.0")
     implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.4.0")
-    implementation("androidx.navigation:navigation-compose:2.9.5")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.activity:activity-compose:1.9.0")
@@ -86,4 +87,9 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+
 }
---
+++ app/src/main/java/wolf/north/sitzer/repository/UserRepository.kt
@@ -0,0 +1,20 @@
+package wolf.north.sitzer.repository
+
+import wolf.north.sitzer.database.UserDao
+import wolf.north.sitzer.database.UserEntity
+import wolf.north.sitzer.mvvm.model.User
+
+
+//bridge between database entity and User model in mvvm
+class UserRepository(private val userDao: UserDao) {
+
+    suspend fun registerUserWithCredentials(firstName: String, email: String, password: String) {
+        val userEntity = UserEntity(firstName = firstName, email = email, password = password)
+        userDao.insertUser(userEntity)
+    }
+
+    suspend fun loginUser(email: String, password: String): User? {
+        val entity = userDao.loginUser(email, password)
+        return entity?.let { User(it.firstName, it.email, it.password) }
+    }
+}
