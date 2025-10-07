0397c24ecdc0879eacb65980e0d40be8f07febaa
Bartek <bartlomiej00wilk@gmail.com>
2025-10-07 13:58:50 +0200

    res: img added comp: progresscard, workoutcard, onboarding overlay components

---
+++ app/src/main/java/wolf/north/sitzer/comps/WorkoutCard.kt
@@ -0,0 +1,95 @@
+package wolf.north.sitzer.comps
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import wolf.north.sitzer.R
+import wolf.north.sitzer.comps.ui.theme.SitzerTheme
+
+@Composable
+fun WorkoutCard(image: Int, workoutName: String, description: String) {
+    Box(
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Card(
+                modifier = Modifier
+                    .width(360.dp)  // Możesz zwiększyć np. do 400.dp
+                    .height(230.dp) // Mniejsza wysokość, bardziej prostokątna
+                    .padding(bottom = 16.dp),
+                shape = RoundedCornerShape(8.dp),
+                elevation = CardDefaults.cardElevation(6.dp)
+            ) {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Image(
+                        painter = painterResource(image),
+                        contentDescription = "Nasza misja",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.BottomStart)
+                            .fillMaxWidth()
+                            .background(
+                                Brush.verticalGradient(
+                                    colors = listOf(
+                                        Color.Transparent,
+                                        Color.Black.copy(alpha = 0.7f)
+                                    )
+                                )
+                            )
+                            .padding(16.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier.align(Alignment.CenterStart),
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                workoutName,
+                                color = Color.White,
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(
+                                description,
+                                color = Color.White,
+                                fontSize = 16.sp
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true, apiLevel = 34)
+@Composable
+fun WorkoutCardPreview() {
+    SitzerTheme {
+        WorkoutCard(R.drawable.neckpainimg, "Neck", "test test test test")
+    }
+}
