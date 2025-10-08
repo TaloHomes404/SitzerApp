47e2f0c17ea4425fe6f11160bbf4851020a9da2b
Bartek <bartlomiej00wilk@gmail.com>
2025-10-08 14:25:55 +0200

    categories carousel + plans display with filter added

--- app/src/main/java/wolf/north/sitzer/mvvm/view/HomeScreen.kt
+++ app/src/main/java/wolf/north/sitzer/mvvm/view/HomeScreen.kt
@@ -3,12 +3,15 @@ package wolf.north.sitzer.mvvm.view
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Home
@@ -24,10 +27,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,21 +39,37 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import wolf.north.sitzer.R
 import wolf.north.sitzer.comps.CategoriesCarousel
+import wolf.north.sitzer.comps.ExercisePlan
 import wolf.north.sitzer.comps.ProgressCard
 import wolf.north.sitzer.comps.ProgressCardNumberIndicator
 import wolf.north.sitzer.comps.WorkoutCardButtoned
+import wolf.north.sitzer.mvvm.viewmodel.PlansViewModel
 import wolf.north.sitzer.navigation.Screens
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavHostController = rememberNavController()) {
 
-    //State to remember selected category
-    var selectedCategory by remember { mutableStateOf("All") }
+//    //State to remember selected category
+//    var selectedCategory by remember { mutableStateOf("All") }
+//
+//
+//    val allPlans = remember { PlansRepository.getAllPlans() }
+//    val filteredPlans = remember(selectedCategory) {
+//        PlansRepository.getPlansSortByCategory(selectedCategory)
+//    }
+    val viewModel: PlansViewModel = viewModel()
+    val selectedCategory by viewModel.selectedCategory.collectAsState()
+    val filteredPlans = viewModel.getPlansSortByCategory()
+
+
+
+
 
     Scaffold(
         topBar = {
@@ -104,7 +121,7 @@ fun HomeScreen(navController: NavHostController = rememberNavController()) {
                     )
                 }
 
-                Spacer(modifier = Modifier.height(8.dp))
+                Spacer(modifier = Modifier.height(4.dp))
 
                 // 2nd row
                 Row(
@@ -139,13 +156,19 @@ fun HomeScreen(navController: NavHostController = rememberNavController()) {
 
                 CategoriesCarousel(
                     selectedCategory = selectedCategory,
-                    onCategorySelected = { newCategory -> selectedCategory = newCategory })
-                val filteredCategories = if (selectedCategory == "All") {
-                    //allWorkouts //TODO: zaimplementowac filtorwanie planow
-                } else {
-                    // allWorkouts.filter { it.category == selectedCategory }
-                }
+                    onCategorySelected = { viewModel.selectedCategory(it) })
 
+                Spacer(modifier = Modifier.height(8.dp))
+
+                // LazyRow z przefiltrowanymi planami
+                LazyRow(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    contentPadding = PaddingValues(horizontal = 16.dp)
+                ) {
+                    items(filteredPlans) { plan ->
+                        ExercisePlan(plan.imageRes, plan.name, plan.duration, plan.exerciseCount)
+                    }
+                }
             }
         },
         bottomBar = {
