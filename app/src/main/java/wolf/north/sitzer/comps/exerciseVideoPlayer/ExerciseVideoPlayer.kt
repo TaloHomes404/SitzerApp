9557d6a30575e30f069885ef415c6385dd9e8b22
Bartek <bartlomiej00wilk@gmail.com>
2026-02-19 18:12:31 +0100

    ExoPlayer changes for performance, timer pause/play changes, names fix in vm

--- app/src/main/java/wolf/north/sitzer/comps/exerciseVideoPlayer/ExerciseVideoPlayer.kt
+++ app/src/main/java/wolf/north/sitzer/comps/exerciseVideoPlayer/ExerciseVideoPlayer.kt
@@ -1,10 +1,18 @@
 package wolf.north.sitzer.comps.exerciseVideoPlayer
 
-import android.widget.FrameLayout
 import androidx.annotation.OptIn
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
@@ -19,40 +27,61 @@ import androidx.media3.ui.PlayerView
 @Composable
 fun ExerciseVideoPlayer(
     videoRes: Int,
+    isPlaying: Boolean = true,
     modifier: Modifier = Modifier
 ) {
     val context = LocalContext.current
+    var isLoading by remember { mutableStateOf(true) }
+
     val exoPlayer = remember {
         ExoPlayer.Builder(context).build().apply {
-            val uri = RawResourceDataSource.buildRawResourceUri(videoRes)
-            val mediaItem = MediaItem.Builder()
-                .setUri(uri)
-                .build()
-            setMediaItem(mediaItem)
-            prepare()
-            playWhenReady = true
+            //Listener added to player to set loading state to false when video is ready
+            addListener(object : Player.Listener {
+                override fun onRenderedFirstFrame() {
+                    isLoading = false
+                }
+            })
             repeatMode = Player.REPEAT_MODE_ONE
             volume = 0f
         }
     }
 
-    DisposableEffect(videoRes) {
+    LaunchedEffect(videoRes) {
+        isLoading = true
+        val uri = RawResourceDataSource.buildRawResourceUri(videoRes)
+        exoPlayer.setMediaItem(MediaItem.fromUri(uri))
+        exoPlayer.prepare()
+    }
+
+    LaunchedEffect(isPlaying) {
+        exoPlayer.playWhenReady = isPlaying
+    }
+
+    //Unit type used to don't destroy player after video changes
+    DisposableEffect(Unit) {
         onDispose {
             exoPlayer.release()
         }
     }
 
-    AndroidView(
-        modifier = modifier,
-        factory = {
-            PlayerView(context).apply {
-                player = exoPlayer
-                useController = false
-                layoutParams = FrameLayout.LayoutParams(
-                    FrameLayout.LayoutParams.MATCH_PARENT,
-                    FrameLayout.LayoutParams.MATCH_PARENT
-                )
-            }
+    Box(modifier = modifier) {
+        //If video is loading play progress indicator
+        if (isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center),
+                color = MaterialTheme.colorScheme.primary
+            )
         }
-    )
+
+        //Player
+        AndroidView(
+            modifier = Modifier.fillMaxSize(),
+            factory = {
+                PlayerView(context).apply {
+                    player = exoPlayer
+                    useController = false
+                }
+            }
+        )
+    }
 }
