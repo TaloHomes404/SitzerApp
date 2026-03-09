1c7218ed53ef3df2be7bd4b600c5aa83cbe95e89
Bartek <bartlomiej00wilk@gmail.com>
2026-03-09 10:02:54 +0100

    Add localized strings for Topbar, Login, Register, and Profile screens; add `getIconRes` to `MuscleGroup` enum.

--- app/src/main/java/wolf/north/sitzer/enums/ExerciseEnums.kt
+++ app/src/main/java/wolf/north/sitzer/enums/ExerciseEnums.kt
@@ -1,5 +1,7 @@
 package wolf.north.sitzer.enums
 
+import wolf.north.sitzer.R
+
 enum class MuscleGroup {
     NECK,
     SHOULDERS,
@@ -12,6 +14,21 @@ enum class MuscleGroup {
     WRISTS,
     CHEST
 
+}
+
+fun MuscleGroup.getIconRes(): Int {
+    return when (this) {
+        MuscleGroup.NECK -> R.drawable.neck
+        MuscleGroup.SHOULDERS -> R.drawable.shoulders
+        MuscleGroup.BACK -> R.drawable.back
+        MuscleGroup.CORE -> R.drawable.core
+        MuscleGroup.HIPS -> R.drawable.hips
+        MuscleGroup.GLUTES -> R.drawable.glutes
+        MuscleGroup.KNEES -> R.drawable.knees
+        MuscleGroup.LEGS -> R.drawable.legs
+        MuscleGroup.WRISTS -> R.drawable.wrists
+        MuscleGroup.CHEST -> R.drawable.chest
+    }
 }
 
 enum class Difficulty {
